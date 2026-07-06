8a524a538533c7388222460d5f6ec8fec3859f1b
Cleydson Santos <cleydsonx7santos@gmail.com>
2026-07-06 12:05:43 -0300

    Simplifica carregamento do Clarity

--- lp-hero-partes-main/app/layout.tsx
+++ lp-hero-partes-main/app/layout.tsx
@@ -65,15 +65,10 @@ export default function RootLayout({
         {children}
         <GoogleAnalytics gaId="G-RNLLXTJ6J3" />
         <Analytics />
-        <Script id="clarity" strategy="afterInteractive">{`
-          if (window.location.hostname !== 'localhost') {
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "xi7plaoebn");
-          }
-        `}</Script>
+        <Script
+          src="https://www.clarity.ms/tag/xi7plaoebn"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
