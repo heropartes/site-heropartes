7b01da0aa2d2e7f1b30a79e391e52c13bf86a1d9
Cleydson Santos <cleydsonx7santos@gmail.com>
2026-07-06 12:51:05 -0300

    Remove try-catch do Clarity

--- lp-hero-partes-main/app/layout.tsx
+++ lp-hero-partes-main/app/layout.tsx
@@ -65,10 +65,13 @@ export default function RootLayout({
         {children}
         <GoogleAnalytics gaId="G-RNLLXTJ6J3" />
         <Analytics />
-        <Script
-          src="https://www.clarity.ms/tag/xi7plaoebn"
-          strategy="afterInteractive"
-        />
+        <Script id="clarity" strategy="afterInteractive">{`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "xi7plaoebn");
+        `}</Script>
       </body>
     </html>
   );
